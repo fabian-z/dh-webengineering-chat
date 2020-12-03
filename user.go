77eb0b8ddfe6fc9754eb710b6e89e77c679644e0
Fabian Zaremba <fabian@youremail.eu>
2020-12-03 19:30:53 +0100

    Add basic grid layout, fix session bug

--- user.go
+++ user.go
@@ -8,7 +8,7 @@ import (
 	"github.com/google/uuid"
 )
 
-func newUserSession() string {
+func newUserSession() (string, session.Session) {
 	s := session.Session{
 		User: "Anonymous",
 		UUID: uuid.New(),
@@ -18,7 +18,7 @@ func newUserSession() string {
 		// Error reading randomness, should not happen without OS errors
 		panic(err)
 	}
-	return id
+	return id, s
 }
 
 func addSessionCookie(sessionID string, w http.ResponseWriter) {
@@ -35,18 +35,20 @@ func addSessionCookie(sessionID string, w http.ResponseWriter) {
 
 func getOrCreateUserSession(w http.ResponseWriter, r *http.Request) session.Session {
 	var sessionID string
+	var curSession session.Session
 	// check if a session exists for this client, if not add one and send ID with cookie
 	sessionCookie, err := r.Cookie(sessionCookieName)
 	if err == http.ErrNoCookie {
 		// no cookie sent, add new session
-		sessionID = newUserSession()
+		sessionID, curSession = newUserSession()
 		addSessionCookie(sessionID, w)
-	}
-	curSession, err := session.GetSession(sessionCookie.Value)
-	if err != nil {
-		// expired or invalid cookie sent, add new session
-		sessionID = newUserSession()
-		addSessionCookie(sessionID, w)
+	} else {
+		curSession, err = session.GetSession(sessionCookie.Value)
+		if err != nil {
+			// expired or invalid cookie sent, add new session
+			sessionID, curSession = newUserSession()
+			addSessionCookie(sessionID, w)
+		}
 	}
 
 	return curSession
